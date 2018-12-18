4278c67b564d2b5eab09672cd83c7d3fde781ce8
dj0mlinho <30881187+dj0mlinho@users.noreply.github.com>
2018-12-18 07:37:42 +0100

    encapsulation - object in self-invoking function

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function loadVideo() {
 
 
 //ENCAPSULATION STYLE (da se vidi u konzoli)
-
+/*
 let videoPlayer = {
   playImg: document.querySelector("#playImg"),
   reloadImg: document.querySelector("#reloadImg"),
@@ -50,32 +50,33 @@ let videoPlayer = {
 }
 
 videoPlayer.init()
+*/
 
-//ENCAPSULATION STYLE (da se ne vidi u konzoli)
+//ENCAPSULATION STYLE in function (da se ne vidi u konzoli)
 
 (function () {
-    let videoPlayer = {
-      playImg: document.querySelector("#playImg"),
-      reloadImg: document.querySelector("#reloadImg"),
-      video: document.querySelector("video"),
-      init: function () {
-        this.playImg.addEventListener('click', this.startVideo)
-        this.reloadImg.addEventListener('click', this.loadVideo)
-      },
-      startVideo: function () {
-        if (this.getAttribute('src') == 'img/play.png') {
-          videoPlayer.video.play()
-          this.setAttribute('src', "img/pause.jpg")
-        } else {
-          videoPlayer.video.pause()
-          videoPlayer.playImg.setAttribute('src', "img/play.png")
-        }
-      },
-      loadVideo: function () {
-        videoPlayer.video.load()
+  let videoPlayer = {
+    playImg: document.querySelector("#playImg"),
+    reloadImg: document.querySelector("#reloadImg"),
+    video: document.querySelector("video"),
+    init: function () {
+      this.playImg.addEventListener('click', this.startVideo)
+      this.reloadImg.addEventListener('click', this.loadVideo)
+    },
+    startVideo: function () {
+      if (this.getAttribute('src') == 'img/play.png') {
+        videoPlayer.video.play()
+        this.setAttribute('src', "img/pause.jpg")
+      } else {
+        videoPlayer.video.pause()
         videoPlayer.playImg.setAttribute('src', "img/play.png")
       }
+    },
+    loadVideo: function () {
+      videoPlayer.video.load()
+      videoPlayer.playImg.setAttribute('src', "img/play.png")
     }
+  }
 
-    videoPlayer.init()
-  }()
+  videoPlayer.init()
+})()
